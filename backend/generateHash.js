e59532a4bcf10f215b715584029066dcf7ee89d5
Tech24cd <tech.cd2024@gmail.com>
2025-09-05 12:05:49 +0200

    Delete backend directory

--- backend/generateHash.js
+++
@@ -1,8 +0,0 @@
-// generate-hash.js
-const bcrypt = require("bcryptjs");
-
-const motDePasse = "admin123";
-
-bcrypt.hash(motDePasse, 10).then((hash) => {
-  console.log("🔐 Nouveau hash pour admin123 :", hash);
-});
